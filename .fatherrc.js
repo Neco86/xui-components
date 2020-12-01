fd3f54b5040dfe2ee460825aacd17331ad8d62f2
Tengfei Xi <xitengfei@github.com>
2020-12-01 10:57:01 +0800

    add demo component

---
+++ .fatherrc.js
@@ -0,0 +1,22 @@
+export default {
+  target: 'browser',
+  entry: 'src/index.ts',
+  esm: 'babel',
+  cjs: 'babel',
+  runtimeHelpers: true,
+  extraBabelPlugins: [
+    ['babel-plugin-import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }]
+  ],
+  autoprefixer: {
+    browsers: ['ie>9', 'Safari >= 6'],
+  },
+  doc: {
+    themeConfig: { mode: 'light' },
+    base: '/',
+    menu: []
+  },
+};
